02d64980eca075aadc440fea6f680dda44f4c49e
sazzer <graham@grahamcox.co.uk>
2014-12-10 12:20:17 +0000

    Fixed some warnings

--- examples/ncrs_size.rs
+++ examples/ncrs_size.rs
@@ -1,6 +1,6 @@
 extern crate ncrs;
 
 fn main() {
-    let mut ncrs = ncrs::NCRS::new();
+    let ncrs = ncrs::NCRS::new();
     println!("NCRS UI is {}x{} in size", ncrs.width(), ncrs.height());
 }
